b1d5bb25cf8ca7947d0266101529ad50bc39369b
Hannah Werman <hanna.w28@gmail.com>
2019-05-03 00:54:46 +0000

    lesson 12 - pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,8 @@
 class PlacesController < ApplicationController
 
     def index
-        @places = Place.all
+        # @places = Place.all
+        @pagy, @places = pagy(Place.all, items: 5)
     end
 
 end
